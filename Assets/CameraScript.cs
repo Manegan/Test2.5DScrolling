0f69fbe9a9cb5136b90e47f1c59dc23d380cf61c
Yenrick Iskandaroff <yenrick.iskandaroff-ext@socgen.com>
2019-02-08 19:49:59 +0100

    first commit

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject MovementSpace;
+    public GameObject Follow;
+    public float Distance;
+
+    public float smoothSpeed = 0.25f;
+    public Vector3 offset;
+    
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+        {
+            if (hit.collider.tag.Equals("MovementSpace"))
+            {
+                Vector3 rotation = transform.position - (hit.normal * -1);
+                transform.rotation = Quaternion.LookRotation(-hit.normal);
+            }
+        }
+        if (Mathf.Abs(transform.position.magnitude - Follow.transform.position.magnitude) > Distance)
+        {
+
+        } else if (Mathf.Abs(transform.position.magnitude - Follow.transform.position.magnitude) < Distance)
+        {
+
+        }
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, Follow.transform.position + offset, smoothSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/CubeController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    [Range(0, 1)]
+    public float speed;
+    public GameObject MovementSpace;
+    public GameObject SpaceConnector;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            rb.velocity = Vector3.up * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = Vector3.right * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.velocity = Vector3.down * speed;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rb.velocity = Vector3.left * speed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Z) ||
+            Input.GetKeyUp(KeyCode.D) ||
+            Input.GetKeyUp(KeyCode.S) ||
+            Input.GetKeyUp(KeyCode.Q))
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(SpaceConnector.transform.position, SpaceConnector.transform.forward, out hit))
+        {
+            if (hit.transform.tag.Equals("MovementSpace"))
+            {
+                if (hit.distance > 11)
+                {
+                    transform.position += -hit.normal * Time.fixedDeltaTime;
+                }
+                else if (hit.distance < 9)
+                {
+                    transform.position += hit.normal * Time.fixedDeltaTime;
+                }
+            }
+        }
+    }
+}
